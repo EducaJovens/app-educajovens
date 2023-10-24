69276a1bf503092bd6658e3774a4694fc9bcfd4a
Ana Paula <aninha10.desouza@gmail.com>
2023-10-24 20:57:07 -0300

    Initial Commit

---
+++ Educa_Jovens_IOSUITests/Educa_Jovens_IOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Educa_Jovens_IOSUITestsLaunchTests.swift
+//  Educa_Jovens_IOSUITests
+//
+//  Created by user on 24/10/23.
+//
+
+import XCTest
+
+final class Educa_Jovens_IOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
