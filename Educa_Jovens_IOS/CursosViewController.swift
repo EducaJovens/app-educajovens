d9da0dda7ee31a82b31bc8a55065a651ae37349c
Ana Paula <aninha10.desouza@gmail.com>
2023-10-25 20:21:14 -0300

    Primeiro comia

---
+++ Educa_Jovens_IOS/CursosViewController.swift
@@ -0,0 +1,72 @@
+import UIKit
+
+class CursosViewController: UIViewController {
+    private let curso = [
+        "Finacas",
+        "Abrir conta",
+        "Investimento"
+    ]
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 18, weight:.bold)
+        label.textColor = .black
+        label.numberOfLines = 0
+        label.text = "Cursos"
+        return label
+    }()
+    
+    private let tableView:UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.separatorStyle = .none
+        return table
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+    }
+    
+    private func setupView(){
+        view.backgroundColor = .white
+        tableView.dataSource = self
+        addViewsInHierarchy()
+        setupConstraints()
+    }
+    
+    private func addViewsInHierarchy(){
+        view.addSubview(titleLabel)
+        view.addSubview(tableView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+extension CursosViewController:UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        curso.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = curso[indexPath.row]
+        
+        return cell
+    }
+}
